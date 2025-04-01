8f3841e3788fbb13cae4d4ad9df92e18b9274105
Stefan Hager <96697204+s-hager@users.noreply.github.com>
2025-04-01 15:46:37 +0200

    fix supported_cpus

--- src/Blacksmith.cpp
+++ src/Blacksmith.cpp
@@ -41,13 +41,11 @@ int check_cpu() {
       "i7-8086",
       "i7-8700",
       "i7-9700",
-      "i7-9900"
-      "i7-9900"
+      "i7-9900",
 
       // ours
-      "i9-11900K" // hammertest 13
-      "i9-10900K" // hammertest 02
-
+      "i9-11900K", // hammertest 13
+      "i9-10900K"  // hammertest 02
   };
 
   bool cpu_supported = false;
